93a1c645cceb0d6040f9674b9db17a0fcf71a553
josue benitez <jbenitez.capacitacion@gmail.com>
2023-11-09 23:27:06 -0400

    Agregar archivos de proyecto.

---
+++ ManejoBaseDatos/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ManejoBaseDatos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            conectandonos();
+        }
+        public static void conectandonos()
+        {
+            string cadenaConexion = @"Server=.\MSSQLSERVER2022; DataBase=Instituto X; User=sa;Password=;";
+        }
+    }
+}
